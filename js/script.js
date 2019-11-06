9a82599273d25c5d49b42c7bac136d9cb96dc170
ase1ia <56377522+ase1ia@users.noreply.github.com>
2019-11-06 20:30:54 +0600

    First commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+
+$(document).ready(function(){
+
+    $(".owl-carousel").owlCarousel();
+
+    $('.burger-menu').on('click', function (e) {
+        e.preventDefault();
+        $('.burger-menu').toggleClass('burger-open');
+        $ ('.menu') .toggleClass('menu-show')
+    });
+
+});
